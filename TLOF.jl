8f341dfd42522490c6d3d5c2158b34aab07f2259
Fatemeh Heydari <102415660+FH96@users.noreply.github.com>
2022-11-20 19:55:13 -0800

    Update TLOF.jl

--- TLOF.jl
+++ TLOF.jl
@@ -1,8 +1,10 @@
 
+
 using JuMP, Ipopt , SBML , LinearAlgebra
 
 
 # flux_estimation is a dataframe (or ?) that has two columns, the first one contains the name of the reactions and the second one flux values
+
 
 
 function TLOF(metabolic_model,lambda,flux_estimation,module_flux,selected_rxns,carbon_uptake_rxn,carbon_uptake_rate,sd=0)
@@ -21,40 +23,46 @@ function TLOF(metabolic_model,lambda,flux_estimation,module_flux,selected_rxns,c
     #getting the stoichiometric matrix 
     _,_,s_matrix=stoichiometry_matrix(metabolic_model)
   
-   
+    #the name of the reactions are extracted as a vector which will be used in the next steps
     reaction_names_keySet=keys(metabolic_model.reactions)
     reaction_names=[]
     for item in reaction_names_keySet
         push!(reaction_names,item)
 
     end
+
     #finding the index of the carbon source uptake reaction 
     carbon_uptake_indx=findall(x->x==carbon_uptake_rxn,reaction_names )
 
-    #Defining the optimization model and the solver 
-    model=JuMP.Model(optimizer_with_attributes(Ipopt.Optimizer))
     
-    #The name of the reactions are modifed to be the same as metabolic model
+    #The name of the reactions are modified to be the same as the names in the metabolic model
     modified_rxn_names=[]
     for item in rxn_names
         push!(modified_rxn_names,string("R_",item))
     end    
+
     #extracting the indices of reactions whose measured flux are available (this information will be used in the objective function )    
     pre_indcs_of_measured_rxns=[findall(x->x==item,reaction_names) for item in modified_rxn_names]
     indcs_of_measured_rxns=[item[1] for item in pre_indcs_of_measured_rxns ] 
 
 
-    
-    ommited_rxn=setdiff(1:length(metabolic_model.reactions) ,selected_rxns)   
-    ommited_rxn=setdiff(ommited_rxn,carbon_uptake_indx)   
+    #One of the constraints is imposed on reations which are not neither included in potential cellular objective nor carbon upatke reaction, so here their index is determined    
+    omited_rxn=setdiff(1:length(metabolic_model.reactions) ,selected_rxns)   
+    omited_rxn=setdiff(omited_rxn,carbon_uptake_indx)   
 
+    #Defining the optimization model and the solver 
+    model=JuMP.Model(optimizer_with_attributes(Ipopt.Optimizer))
+
+    ###VARIABLES
+
+    #Defining the flux variables 
     @variable(model,-1000<=v[1:length(metabolic_model.reactions) ]<=1000)
     
-    # the dual variables
+    #Defining the dual variables
     @variable(model,u[1:length(metabolic_model.species)] )
     @variable(model,g)
 
-    # defining (the variable associated with the coefficient of each reaction in the objective function
+    # defining the variable associated with the coefficient of each reaction in the objective function
     @variable(model,-1<=c[1:size(selected_rxns,1)]<=1)
 
  
@@ -63,38 +71,38 @@ function TLOF(metabolic_model,lambda,flux_estimation,module_flux,selected_rxns,c
  
  
  
-    ###constraints
+    ###CONSTRAINTS
 
+    #these two constraints are for the new variable define to omit
     @constraint(model,a.>=c)                                           
     @constraint(model,a.>=-c)    
 
     #Constraint regarding irreversible reactions
     @constraint(model,[j in irreversible_indices],0<=v[j]<=1000)                                     
     
-    #constraint regarding the equality of the primal and the dual objective finctions, applied for reactions in potential cellular objective set
+    # the equality of the primal and the dual objective finctions, applied for reactions in potential cellular objective set
     @NLconstraint(model,sum(c[j] * v[selected_rxns[j]] for j in 1:length(selected_rxns)) ==carbon_uptake_rate*g )
 
     #the stoichiometric constraint, coming from the primal
     @constraint(model, s_matrix*v.==0)           
 
     # A constraint from the dual problem, it is applied for reactions in potential cellular objective set
-    @constraint(model,[dot(u,s_matrix[:,selected_rxns[j]])-c[j] for j in 1:size(selected_rxns,1) ].>=0)        #5 ++
+    @constraint(model,[dot(u,s_matrix[:,selected_rxns[j]])-c[j] for j in 1:size(selected_rxns,1) ].>=0)        
 
-    # Another constraint from the dual problem, it is applied for reactions which are not in potential cellular objective set
-    #@constraint(model,[dot(u,s_matrix_df[:,j+1]) for j in the_rest_indices].>=0)                                 #6 +
-    @constraint(model, [j in ommited_rxn], u' * s_matrix[:, j] >= 0)                                   #6 ?
+    # Another constraint from the dual problem, it is applied for reactions which are not in potential cellular objective set                                
+    @constraint(model, [j in omited_rxn], u' * s_matrix[:, j] >= 0)                                  
 
-    #constraint on eschange flux for carbon source, if no standard deviation is given to the function, it will be an equality constraint
+    #constraint on exchange flux for carbon source, if no standard deviation is given to the function, it will be an equality constraint
     @constraint(model,v[carbon_uptake_indx].>=[carbon_uptake_rate]-sd)                                                              
     @constraint(model,v[carbon_uptake_indx].<=[carbon_uptake_rate]-sd)                                                              
 
-
+    #this constraint is applied for the carbon uptake reaction
     @constraint(model,dot(u,s_matrix[:,carbon_uptake_indx[1]])+g>=0)                                               #7 +
 
-    #the length of the c vector, which is actually the number of nonzero elements, is calculated for normalization purposes  
+    #the number of reactions regarded as potential cellular objective, is calculated for normalization purposes  
     n_selected=size(selected_rxns,1)
 
-    #the length of the c vector, which is actually the number of nonzero elements, is calculated for normalization purposes  
+    #the number of measured fluxes, is calculated for normalization purposes  
     n_estimated=size(flux_estimation,1)
 
     @NLobjective(model,Min,sqrt(sum((sum(module_flux[j,i]*v[indcs_of_measured_rxns[i]] for i in 1:size(indcs_of_measured_rxns,1))- flux_estimation[j,2])^2 for j in 1:size(flux_estimation,1 )))+(n_estimated/n_selected)*lambda*(sum(a[k] for k in 1:size(c,1))))
@@ -108,12 +116,21 @@ function TLOF(metabolic_model,lambda,flux_estimation,module_flux,selected_rxns,c
             set_start_value(v[i],rand(-1000:0.01:1000))
         end
     end
+
     JuMP.optimize!(model)
 
+    #
+    if JuMP.raw_status(model)!="Solve_Succeeded"
+        println(JuMP.termination_status(model))
+    
+    end
 
-    return JuMP.value.(c)
+
+    return JuMP.value.(c) ,JuMP.objective_value(model)
 end
 
-#bar(c)
+
+
+
 
 
